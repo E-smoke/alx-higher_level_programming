c8eb2e58d6f27f4e4365f397f49c1c5a2ae3b771
Elijah <eachala5@gmail.com>
2023-11-28 15:31:03 -0800

    14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -5,5 +5,5 @@ for i in range(ord('z'), ord('a') - 1, -1):
         print("{}".format(chr(i)), end='')
         a = 0
     else:
-         print("{}".format(chr(i - 32)), end='')
-         a = 1
+        print("{}".format(chr(i - 32)), end='')
+        a = 1
