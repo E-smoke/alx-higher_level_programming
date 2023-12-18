a18588e2cb0a5485130f4d46699d3dcb0c7f00d2
Elijah <eachala5@gmail.com>
2023-12-18 13:04:51 -0800

    88

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,5 +4,6 @@ def safe_function(fct, *args):
     try:
         r = fct(*args)
     except Exception as err:
+        r = None
         print("Exception: {}".format(err), file=sys.stderr)
     return r
