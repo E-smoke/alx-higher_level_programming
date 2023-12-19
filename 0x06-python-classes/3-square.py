0311850a58580e79a3fc17464351cef97b73825a
Elijah <eachala5@gmail.com>
2023-12-19 10:53:15 -0800

    3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""this module defines an empty class name square"""
+
+
+class Square:
+    """this is an empty class called Square"""
+    def __init__(self, size=0):
+        """initfunction to initialize size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns area of a square"""
+        return self.__size ** 2
