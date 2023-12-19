53e9a89259dfd270f878bad0cee337205ca33a5f
Elijah <eachala5@gmail.com>
2023-12-19 10:05:09 -0800

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this module defines an empty class name square"""
+
+
+class Square:
+    """this is an empty class called Square"""
+    def __init__(self, size):
+        """init function to initialize size"""
+        self.size = size
