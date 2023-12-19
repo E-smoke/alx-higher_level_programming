9cb0b7f239480a591d9b5a1b753e364061bf626f
Elijah <eachala5@gmail.com>
2023-12-19 11:14:29 -0800

    4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""this module defines an empty class name square"""
+
+
+class Square:
+    """this is an empty class called Square"""
+    def __init__(self, size=0):
+        """initfunction to initialize size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns area of a square"""
+        return self.__size ** 2
+
+    def size(self):
+        """To retrieve value of size"""
+        return self.__size
+
+    def size(self, new_size):
+        """To set the value of size"""
+        if type(new_size) != int:
+            raise TypeError("size must be an integer")
+        if new_size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = new_size
