d3de8fab1888cece782f5c50ebf4d63e64ef6dec
Elijah <eachala5@gmail.com>
2023-12-19 13:12:42 -0800

    05

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -32,8 +32,9 @@ class Square:
 
     def my_print(self):
         """prints the square"""
+        if self.__size = 0:
+            print()
         for i in range(self.__size):
             for j in range(self.__size):
                 print('#', end='')
             print()
-        print()
