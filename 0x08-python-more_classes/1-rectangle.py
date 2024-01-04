85c23636101e096bc33c8332706b706446e408b9
Elijah <eachala5@gmail.com>
2024-01-04 07:56:05 -0800

    1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""rectangle class"""
+
+
+class Rectangle:
+    """class Rectangle"""
+
+    @property
+    def width(self):
+        """return width"""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        """return width"""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        """set width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._height = value
+
+    def __init__(self, width=0, height=0):
+        """init mehtod"""
+        self.width = width
+        self.height = height
