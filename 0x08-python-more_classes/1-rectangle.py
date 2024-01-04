d39f386bbb3dd42a462eac7e9a0e2c971c2c54c8
Elijah <eachala5@gmail.com>
2024-01-04 07:59:57 -0800

    01

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -28,12 +28,12 @@ class Rectangle:
     def height(self, value):
         """set width"""
         if type(value) != int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self._height = value
 
     def __init__(self, width=0, height=0):
         """init mehtod"""
-        self._width = width
-        self._height = height
+        self.width = width
+        self.height = height
