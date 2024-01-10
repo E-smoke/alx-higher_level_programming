1cfd4fcefe3027d2c78eeef45c9e9ef5469a110b
Elijah <eachala5@gmail.com>
2024-01-10 09:19:37 -0800

    t

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -11,10 +11,10 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """init method"""
 
-        BaseGeometry.integer_validator(self, "width", width)
-        BaseGeometry.integer_validator(self, "height", height)
         self.__width = width
         self.__height = height
+        BaseGeometry.integer_validator(self, "width", self.__width)
+        BaseGeometry.integer_validator(self, "height", self.__height)
 
     def area(self):
         """arear"""
