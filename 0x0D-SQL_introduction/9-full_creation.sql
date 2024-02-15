e54473b20644f0516c8226f787c93c794eaf1e65
Elijah <eachala5@gmail.com>
2024-02-15 06:01:21 -0800

    9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,8 +4,8 @@ id INT,
 name VARCHAR(256),
 score INT);
 INSERT INTO second_table
-VALUES
-(1, “John”, 10),
-(2, “Alex”, 3),
-(3, “Bob”, 14),
-(4, “George”, 8);
+VALUES 
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
