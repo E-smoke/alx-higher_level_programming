a72c1eb743ae846798e96f3f550e9a27982c2213
Elijah <eachala5@gmail.com>
2024-03-11 20:17:16 -0700

    7

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 let argv = process.argv;
 argv = argv[2];
-if (!argv) {
-  console.log('Not a number');
-} else if (isNaN(parseInt(argv))) {
+if (!argv || isNaN(parseInt(argv))) {
   console.log('Not a number');
 } else {
   console.log(`My number: ${argv}`);
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -7,6 +7,7 @@ if (!x) {
   console.log('Missing number of occurrences');
 } else {
   x = Number(x);
+  x = x > 0 ? x : 0;
   for (x; x > 0; --x) {
     console.log('C is fun');
   }
