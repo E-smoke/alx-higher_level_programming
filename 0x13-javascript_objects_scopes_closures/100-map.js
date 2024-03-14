6e9c34552f5f4a7a5623a426631491aabc27f233
Elijah <eachala5@gmail.com>
2024-03-13 19:51:08 -0700

    eee

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
-const callback = function (l) {
-  return l * list.indexOf(l);
-};
 console.log(list);
-console.log(list.map(callback));
+console.log(list.map((item, index) => item * index));
