89a10fcc4490e4c567b078907c70e9ee0044b5df
Elijah <eachala5@gmail.com>
2024-03-25 18:47:29 -0700

    5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''mysqldb 5'''
+
+
+import sys
+from MySQLdb import connect
+
+if __name__ == "__main__":
+    mydb = connect(host="localhost", port=3306, user=sys.argv[1],
+                   passwd=sys.argv[2], database=sys.argv[3])
+    mycursor = mydb.cursor()
+    query1 = "SELECT GROUP_CONCAT(name SEPARATOR ', ') FROM cities WHERE state"
+    query2 = "_id = (SELECT id FROM states WHERE name = %s LIMIT 1) "
+    query3 = "ORDER BY id ASC"
+    mycursor.execute(query1 + query2 + query3, (sys.argv[4],))
+    result = mycursor.fetchone()
+    print(result[0])
+    mycursor.close()
+    mydb.close()
