0aff4070519838a24790b46c8a077b120487d50c
Elijah <eachala5@gmail.com>
2024-03-26 04:04:50 -0700

    11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''sqlalchemy 3'''
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    pattern = "mysql://{}:{}@localhost:3306/{}"
+    engine = create_engine(pattern.format(sys.argv[1],
+                           sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    obj = State(name='Louisiana')
+    result = session.add(obj)
+    session.commit()
+    print(obj.id)
+    session.close()
